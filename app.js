4ec24f958fdc3d586074e12a22dd2e8f401a1904
Cassul <aysa.garden@gmail.com>
2019-08-15 21:11:17 +1000

    connect server to mongo

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require("express")
-const path = require("path")
-const routes = require('./routes')
- 
-const app = express()
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const MongoClient = require("./mongo");
+
+const app = express();
 // Set the default views directory to html folder
-app.set('views', path.join(__dirname, 'html'))
- 
+app.set("views", path.join(__dirname, "html"));
+
 // Set the folder for css & java scripts
-app.use(express.static(path.join(__dirname,'css')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
- 
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
 // Set the view engine to ejs
-app.set('view engine', 'ejs')
- 
-app.use('/', routes)
- 
-app.listen(process.env.PORT || 7777, () => {
-    console.log("Server is running at localhost:7777")
-})
+app.set("view engine", "ejs");
+
+app.use("/", routes);
+
+server = app.listen(process.env.PORT || 7777, () => {
+  console.log("Server is running at localhost:7777");
+});
